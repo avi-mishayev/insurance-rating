4d0107ea3ff36c1fa42ef6897b29618d21bc1f7a
Avi <avim@auroraview.com>
2023-02-13 20:37:16 +0200

    added PolicyJsonConverter for efficient deserialize

---
+++ Converter/PolicyJsonConverter.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Nipendo.Evaluation
+{
+    public class PolicyJsonConverter : JsonConverter<Policy>
+    {
+        private static readonly Dictionary<PolicyType, Policy> Policies = new Dictionary<PolicyType, Policy>
+                {
+                    { PolicyType.Health, new PolicyHealth() },
+                    { PolicyType.Travel, new PolicyTravel() },
+                    { PolicyType.Life, new PolicyLife() },
+                };
+
+        public override Policy ReadJson(JsonReader reader, Type objectType, Policy existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var jsonObject = JObject.Load(reader);
+            var target = Create(objectType, jsonObject);
+            if (target == null)
+            {
+                return null;
+            }
+            serializer.Populate(jsonObject.CreateReader(), target);
+            return target;
+        }
+
+        public override void WriteJson(JsonWriter writer, Policy value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected Policy Create(Type objectType, JObject jsonObject)
+        {
+            var typeName = jsonObject["type"].ToString();
+            bool isConvertSucceeded = Enum.TryParse(typeName, out PolicyType policyType);
+            if (isConvertSucceeded)
+            {
+                return Policies[policyType];
+            }
+            Console.WriteLine("Unknown policy type");
+            return null;
+        }
+    }
+}
